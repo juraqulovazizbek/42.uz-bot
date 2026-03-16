e241775dd468400078ea556d39bd9ec7c7ca4798
juraqulovazizbek <juraqulovazizbek85@gmail.com>
2026-03-16 11:36:05 +0500

    cutom auth user model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'telegram_username')
+    search_fields = ('chat_id', 'telegram_username')
+    list_filter = ('telegram_username',)
+    
--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    chat_id = models.CharField(max_length=255, unique=True)
+    telegram_username = models.CharField(max_length=255, blank=True, null=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Foydalanuvchi'
+        verbose_name_plural = 'Foydalanuvchilar'
+
+    def __str__(self):
+        return self.username
